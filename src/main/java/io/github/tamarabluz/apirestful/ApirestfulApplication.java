a006df427980a4456c46f9cdb32147a970ab7537
Tamara Luz <tamarabluz@gmail.com>
2023-02-06 10:00:21 -0300

    first commit

---
+++ src/main/java/io/github/tamarabluz/apirestful/ApirestfulApplication.java
@@ -0,0 +1,13 @@
+package io.github.tamarabluz.apirestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestfulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/tamarabluz/apirestful/ApirestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.tamarabluz.apirestful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
